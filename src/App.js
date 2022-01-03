42d77c3fd5d16c3efec7bc5a23686aaba35a42ff
lkotlarenko <lkotlarenko@protonmail.com>
2022-01-03 15:27:04 -0300

    Creating component SolarSystem

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import Header from './components/Header';
+import SolarSystem from './components/SolarSystem';
 class App extends React.Component {
   render() {
     return (
-      <Header />
+      <>
+        <Header />
+        <SolarSystem />
+      </>
     );
   }
 }
---
+++ src/components/SolarSystem.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class SolarSystem extends React.Component {
+  render() {
+    return (
+      <div data-testid="solar-system">Test</div>
+    );
+  }
+}
+
+export default SolarSystem;
