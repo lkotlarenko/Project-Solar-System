6af1ff8bfabd3d7842e3cfbba34ddcf6065b6ab2
lkotlarenko <lkotlarenko@protonmail.com>
2022-01-03 15:19:34 -0300

    Creating component Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from 'react';
-
+import Header from './components/Header';
 class App extends React.Component {
   render() {
-    return (<p>Sistema Solar</p>);
+    return (
+      <Header />
+    );
   }
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Header extends React.Component {
+  render() {
+    return (
+      <header><h1>Sistema Solar</h1></header>
+    );
+  }
+}
+
+export default Header;
